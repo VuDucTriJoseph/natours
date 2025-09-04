d8958ee03128c06856c49485875c55f07e0cbf36
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-09-04 19:31:53 +0700

    setting vscode, begin with api

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-const express = require("express");
+import express from 'express';
+import fs from 'fs';
 
-const app =express();
+const app = express();
 
 const port = 3000;
 
-app.get("/",(req,res)=>{
-    res.status(200).json({mess:"hello from server side",app:"mature"});
+app.use(express.json());
+// app.get("/api/v1/tours",(req,res)=>{
+//     res.status(200).json({mess:"hello from server side",app:"mature"});
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    result: tours.length,
+    data: { tours },
+  });
 });
 
-app.listen(port,()=>{
-    console.log(`app running on port ${port}...`);
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+  // res.send('done');
+});
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
 });
