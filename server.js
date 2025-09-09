d85384f0051f563fe3a7f7f9143d722c29dab27c
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-09-09 12:17:43 +0700

    connecting with mongodb alat database

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config({ path: './config.env', quiet: true });
-import app from './app.js';
 
+import app from './app.js';
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATA_PASS);
+// console.log(DB);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
